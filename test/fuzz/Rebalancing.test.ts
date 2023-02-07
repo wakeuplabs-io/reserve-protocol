fd21fd66caa1b4d70d6ca2ff947e965c9c61f27e
pmckelvy1 <pmckelvy1@gmail.com>
2023-02-07 13:44:15 -0500

    get scene to fail.

--- test/fuzz/Rebalancing.test.ts
+++ test/fuzz/Rebalancing.test.ts
@@ -1593,29 +1593,11 @@ describe('The Rebalancing scenario', () => {
   })
 
   it.only('mocks the failure', async () => {
-    await advanceTime(63500)
-    await advanceBlocks(4641)
-    // manageBackingTokens() from: 0x0000000000000000000000000000000000010000 Time delay: 63500 seconds Block delay: 4641
     await scenario.connect(alice).manageBackingTokens()
-    // createToken(0,"\NUL","\NUL") from: 0x0000000000000000000000000000000000010000
     await scenario.connect(alice).createToken(0,"\NUL","\NUL")
-    // *wait*
-    // issue(400623555814142) from: 0x0000000000000000000000000000000000010000
     await scenario.connect(alice).issue(400623555814142)
-    // *wait*
-    await advanceTime(63500)
-    await advanceBlocks(4641)
-    // swapRegisteredAsset(36,0,8421411638574458141681995940662771667477248710467825558969664488109615127440,3,false,false) from: 0x0000000000000000000000000000000000010000
     await scenario.connect(alice).swapRegisteredAsset(36,0,bn('8421411638574458141681995940662771667477248710467825558969664488109615127440'),3,false,false)
-    // saveBasketRange() from: 0x0000000000000000000000000000000000010000
-    await scenario.connect(alice).saveBasketRange()
-    // transferFrom(2,0,0,194966158661769055172184548548508711551232618281959441963245459551130168137) from: 0x0000000000000000000000000000000000010000
-    await scenario.connect(alice).transferFrom(2,0,0,bn('194966158661769055172184548548508711551232618281959441963245459551130168137'))
-    // setUnstakingDelay(431994307821249466151546817196356461862387920072886828741061389722366437) from: 0x0000000000000000000000000000000000010000
-    await scenario.connect(alice).setUnstakingDelay(bn('431994307821249466151546817196356461862387920072886828741061389722366437'))
-    // refreshBasket() from: 0x0000000000000000000000000000000000010000
     await scenario.connect(alice).refreshBasket()
-    // issueTo(1,0) from: 0x0000000000000000000000000000000000010000
     await scenario.connect(alice).issueTo(1,0)
     const invariantHolds = await scenario.echidna_basketRangeSmallerWhenRebalancing()
     console.log('invariant holds', invariantHolds)
